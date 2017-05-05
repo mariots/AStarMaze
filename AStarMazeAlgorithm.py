1f6b0ca884b05897afb6cbe39359b8cc94fdde48
mariot <mariotjimenez@gmail.com>
2017-05-05 00:19:44 -0500

    Working A* search algorithm

--- AStarMazeAlgorithm.py
+++ AStarMazeAlgorithm.py
@@ -4,8 +4,10 @@
 
 def AStarSearch(Maze):
     open, closed = [[mousePosition(M)]], [mousePosition(M)]
+    cheese = cheesePosition(Maze)
     print("Open: ", open)
     print("Closed: ", closed)
+    print("Cheese: ", cheese)
     # open is a list of paths, each of which begins at the mousePosition(M)
     # closed is a list of the cells we have found a shortest path to
     while len(open) > 0:
@@ -13,15 +15,17 @@ def AStarSearch(Maze):
         destination = path[len(path) - 1]
         if Maze[destination[0]][destination[1]] == 'c':
             return path  # Found the cheese
-        # throw away the path we just tested     This was in the wrong place
-        # previously
+        # throw away the path we just tested.
         open = open[1:]
         # insert children of path into open list. Each child is a path.
         for cell in adjacentCells(destination, Maze):
             if cell not in closed:
                 # This line did not change the open list p
-                open = insert(path + [cell], open, cheesePosition(Maze))
+                # print("path + cell: ", path, [cell])
+                # print("open: ", open)
+                open = insert(path + [cell], open, cheese)
         closed = closed + [destination]  # and add its last cell to closed list
+        print("Closed: ", closed)
     return None
 
 
@@ -56,11 +60,11 @@ def adjacentCells(destination, Maze):
     if(i < len(Maze) - 1):
         if M[i + 1][j] != 'x':
             cells += [(i + 1, j)]
-    if(i < len(Maze) - 1):
+    if(j < len(Maze) - 1):
         if M[i][j + 1] != 'x':
             cells += [(i, j + 1)]
 
-    print("Cells: ", cells)
+    # print("Adjacent Cells: ", cells)
     return cells
 
 
@@ -71,29 +75,34 @@ def adjacentCells(destination, Maze):
 # The estimated total cost of a path is its length plus the manhattan distance
 # from its last cell to c. For extra credit, write this so that it runs in time
 # O(log(len(L))).
-def insert(path, open, cheesePosition):
+def insert(path, open, cheese):
 
     # The estimated total cost of a path is its length plus the manhattanD.
-    #cost = length + manhattanD(?, cheesePosition)
-
-    print("path: ", path)
-    print("open: ", open)
-    print("cheesePosition: ", cheesePosition)
-    return None
+    # cost = length + manhattanD(?, cheesePosition)
+    for i in range(0, len(open)):
+        if len(path) + manhattanD(path[len(path) - 1], cheese) <= len(open) + manhattanD(open[i][i], cheese):
+            return open[0:i] + [path] + open[i:len(open)]
+    return open + [path]
 
 
 # ManhattanD: cell*cell -> int
 # manhattanD(c1,c2) is the Manhattan distance between c1 and c2
 def manhattanD(c1, c2):
-    (i1, j1) = c1
-    (i2, j2) = c2
+    i1, j1 = c1
+    i2, j2 = c2
     return abs(i1 - i2) + abs(j1 - j2)
 
 
-R0 = ['o', 'm', 'x', 'c']
-R1 = ['o', 'o', 'x', 'o']
-R2 = ['x', 'o', 'o', 'o']
-R3 = ['o', 'x', 'o', 'x']
+# R0 = ['o', 'm', 'x', 'c']
+# R1 = ['o', 'o', 'x', 'o']
+# R2 = ['x', 'o', 'o', 'o']
+# R3 = ['o', 'x', 'o', 'x']
+# M = [R0, R1, R2, R3]
+
+R0 = ['o', 'o', 'o', 'o']
+R1 = ['m', 'x', 'o', 'o']
+R2 = ['x', 'x', 'o', 'o']
+R3 = ['c', 'o', 'o', 'o']
 M = [R0, R1, R2, R3]
 
-AStarSearch(M)
+print("Path to the cheese: ", AStarSearch(M))
